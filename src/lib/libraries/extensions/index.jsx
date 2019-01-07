5d1df5868834171c234513679251345b48aa591a
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2019-01-07 10:44:51 -0500

    Add body sensing to extension library

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -90,6 +90,26 @@ export default [
     {
         name: (
             <FormattedMessage
+                defaultMessage="Body Sensing"
+                description="Name for the 'Body Sensing' extension"
+                id="gui.extension.bodysensing.name"
+            />
+        ),
+        extensionId: 'bodySensing',
+        iconURL: videoImage,
+        insetIconURL: videoInsetImage,
+        description: (
+            <FormattedMessage
+                defaultMessage="Sense body positions with the camera."
+                description="Description for the 'Body Sensing' extension"
+                id="gui.extension.bodysensing.description"
+            />
+        ),
+        featured: true
+    },
+    {
+        name: (
+            <FormattedMessage
                 defaultMessage="Video Sensing"
                 description="Name for the 'Video Sensing' extension"
                 id="gui.extension.videosensing.name"
