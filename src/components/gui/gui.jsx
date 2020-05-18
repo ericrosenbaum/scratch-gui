303cc9cc1b7d1b6a8905967755bde433b72c12a6
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2020-05-18 15:55:35 -0400

    Preload and scroll to sound sensing extension

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -294,6 +294,7 @@ const GUIComponent = props => {
                                             canUseCloud={canUseCloud}
                                             grow={1}
                                             isVisible={blocksTabVisible}
+                                            preLoadedExtension={'soundSensing'}
                                             options={{
                                                 media: `${basePath}static/blocks-media/`
                                             }}
